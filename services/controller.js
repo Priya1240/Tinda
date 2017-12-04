5e2be970a3e9bafc02f4d5a5fae50f0819558b90
josephskrzysowski <josephskrzysowski@gmail.com>
2017-12-03 19:03:24 -0500

    hitting the db now WASUP! Yea? WASUP NOW MONGO? Yea that's what I though... okay now lets have some fun

--- services/controller.js
+++ services/controller.js
@@ -16,11 +16,27 @@ const User = require('./User');
 
 router.post('/register', (req, res) => {
   console.log("hit the post route");
+  console.log("req.body here",req.body);
+  var user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) {
+    res.send(err);
+  }else{
+    console.log("saved register form");
+  }
+  })
+  
+  
   res.redirect('/');
   
 })
 
 module.exports=router;
+
+
+
+
+
 
 
 
